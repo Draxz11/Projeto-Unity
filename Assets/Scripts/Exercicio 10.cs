5d6b7ab3d84c6d12a4ec3aa5155f2948fe66867c
Draxz11 <miguelsinhoreti7@gmail.com>
2024-11-07 15:26:21 -0300

    feat: Exercicio 10 Concluido com Sucesso

--- Assets/Scripts/Exercicio 10.cs
+++ Assets/Scripts/Exercicio 10.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 using UnityEngine;
 using static UnityEditor.Progress;
 
@@ -11,9 +12,34 @@ public class Exercicio10 : MonoBehaviour
     //que têm efeitos diferentes dependendo do tipo coletado.Use um
     //switch case para determinar o efeito de cada tipo de item.
 
+    [SerializeField] string tipoItem;
+    [SerializeField] int moeda = 10;
+    [SerializeField] bool escudo = false;
+    [SerializeField] bool pocaoVida = false;
+
     void Start()
     {
-        
+        switch (tipoItem)
+        {
+            case "Moeda":
+                moeda++;
+                print("Moeda Coletada" + moeda);
+                break;
+
+            case "Escudo":
+                escudo = true;
+                print("Escudo Ativo");
+                break;
+
+            case "Poção":
+                pocaoVida = true;
+                print("Poção de Vida Coletada");
+                break;
+
+            default:
+                Debug.Log("Item desconhecido!");
+                break;
+        }
     }
 
     void Update()
