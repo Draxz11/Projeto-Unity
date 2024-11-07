246604c9e23d33599ea454b256756fe8fd8e395e
Draxz11 <miguelsinhoreti7@gmail.com>
2024-11-07 15:36:19 -0300

    feat: Todos os Exercicios Concluidos com Sucesso !

--- Assets/Scripts/Exercicio 11.cs
+++ Assets/Scripts/Exercicio 11.cs
@@ -12,9 +12,35 @@ public class Exercicio11 : MonoBehaviour
     //Use switch case para definir as respostas baseadas no estado do
     //jogador.
 
+    [SerializeField] int estado;
+
     void Start()
     {
-        
+        switch(estado)
+        {
+            case 1:
+                print("Dá para fazer com ternario");
+                break;
+            case 2:
+                print("Sextouuu");
+                break;
+
+            case 3:
+                print("Cade meu aumento ?");
+                break ;
+
+            case 4:
+                print("Cade o Thiagoo??");
+                break;
+
+            case 5:
+                print("Para de correr no corredor");
+                break;
+
+            default:
+                print("Não tenho nada a dizer sobre isso!");
+                break;
+        }
     }
 
     void Update()
