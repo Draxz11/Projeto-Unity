07602119a83addbd578b7bd5b78722f5dd0d3112
Draxz11 <miguelsinhoreti7@gmail.com>
2024-11-12 15:43:08 -0300

    feat: Exercicio 4 completo

---
+++ Assets/Scripts/Atividade 2/Exer04.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Exer04 : MonoBehaviour
+{
+    /*(Pontuação em combos) Cada combo realizado aumenta em 10
+    pontos. Exiba a pontuação total após 7 combos.*/
+
+    [SerializeField] int pontuacao = 0;
+    void Start()
+    {
+        for (int combo = 1; combo <= 7; combo++)
+        {
+            //pontuacao = pontuacao + 10
+            pontuacao += 10;
+        }
+
+        print("Pontuação total: " + pontuacao);
+    }
+
+    void Update()
+    {
+        
+    }
+}
