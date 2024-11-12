51fac203e552ba48e8828edad5083e178c153ae9
Draxz11 <miguelsinhoreti7@gmail.com>
2024-11-12 16:52:06 -0300

    feat: Exercicio 7 completo

--- Assets/Scripts/Atividade 2/Exer06.cs
+++ Assets/Scripts/Atividade 2/Exer06.cs
@@ -2,39 +2,41 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Exercicio06repeticao : MonoBehaviour
+public class Exer06 : MonoBehaviour
 {
     /*(Fizz Buzz) Para cada número entre 0 e 10000, escreva apenas
     “fizz” no console quando for divisível por 3, “buzz” quando for
     divisível por 5 e “fizz buzz” quando for divisível pelos dois. Se o
     número não for divisível por nenhum deles, escreva-o no console.*/
 
-    [SerializeField] int num = 1;
+
+
     void Start()
     {
-        for (num = 1; num < 1000; num++)
+        //for(inicialização; condição; incrementar/decrementar
+        for (int i = 0; i < 10000; i++)
         {
-            if (num % 3 == 0 && num % 5 == 0)
+            if (i % 3 == 0 && i % 5 == 0)
             {
-                print(num + "fizz buzz");
+                print("Fizz buzz");
             }
-            else if (num % 3 == 0)
+            else if (i % 5 == 0)
             {
-                print(num + "buzz");
+                print("buzz");
             }
-            else if (num % 5 == 0)
+            else if (i % 3 == 0)
             {
-                print(num + "fizz");
+                print("fizz");
             }
-            else
+            else 
             {
-                print(num + "esse número não é divisível nem por 3 nem por 5");
+                print(i);
             }
         }
     }
 
     void Update()
     {
-
+        
     }
 }
---
+++ Assets/Scripts/Atividade 2/Exer07.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Exer07 : MonoBehaviour
+{
+    //    7. (Maior número em uma lista) Dada uma lista de números,
+    //encontre o maior valor.
+
+    void Start()
+    {
+        //
+    }
+
+    void Update()
+    {
+        
+    }
+}
