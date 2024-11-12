835b142e3e81a52b74cf7c59335057b0dc654fcf
Draxz11 <miguelsinhoreti7@gmail.com>
2024-11-12 17:11:16 -0300

    feat: Exercicio 7 completo

--- Assets/Scripts/Atividade 2/Exer07.cs
+++ Assets/Scripts/Atividade 2/Exer07.cs
@@ -6,10 +6,25 @@ public class Exer07 : MonoBehaviour
 {
     //    7. (Maior número em uma lista) Dada uma lista de números,
     //encontre o maior valor.
+    
+   
+    int[] numeros = { 3, 5, n, 7, 10, 68, 23, 71, 18, 97};
+    int maior;
 
     void Start()
     {
-        //
+        foreach (var item in numeros) 
+        {
+            if (maior == 0) 
+                maior = item;
+
+            if (item > maior)
+            {
+                maior = item;
+            }
+        }
+
+        print("O maior número é: " + maior);
     }
 
     void Update()
